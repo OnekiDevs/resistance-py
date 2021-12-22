034ec0e7416f01cd85df3021fd3a302decf5ee95
Gael <69738055+Gael-devv@users.noreply.github.com>
2021-12-22 16:07:52 -0500

    Delete message after timeout and received gift

--- python/cogs/events/christmas.py
+++ python/cogs/events/christmas.py
@@ -7,18 +7,27 @@ import utils
 class Gift(utils.discord.ui.View):
     def __init__(self):
         super().__init__()
-        self.rewards = [
+        self.received_gift = False
+    
+    @property
+    def rewards(self):
+        return utils.random.sample([
             lambda interaction: (await interaction.response.send_message("Toma 🍪, una galleta :D", ephemeral=True) for _ in "_").__anext__(),
-        ]
-        self.punishments = [
+        ], k=1)[0]
+        
+    @property
+    def punishments(self):
+        return utils.random.sample([
             lambda interaction: (await interaction.response.send_message("\*le pega*", ephemeral=True) for _ in "_").__anext__()
-        ]
+        ], k=1)[0]
     
     @utils.discord.ui.button(label='Abrir', style=utils.discord.ButtonStyle.red)
     async def to_open(self, button: utils.discord.ui.Button, interaction: utils.discord.Interaction):
-        reward_or_punishment = utils.random.sample(utils.random.choice([self.rewards, self.punishments]), k=1)[0]
-
-        await reward_or_punishment(interaction)
+        func = utils.random.choice([self.rewards, self.punishments])
+        await func(interaction)
+        self.received_gift = True
+        
+        await interaction.delete_original_message()
         self.stop()
 
 
@@ -51,7 +60,14 @@ class Christmas(utils.commands.Cog):
         # Select channel
         self.guild: utils.discord.Guild = await self.bot.fetch_guild(850338969135611924)
         channel = await self.guild.fetch_channel(utils.random.choice(self.allowed_channels))
-        await channel.send(file=utils.discord.File(fp="resource/img/Gift-Box.jpg"), view=Gift())
+        
+        # Gift interaction
+        view = Gift()
+        message = await channel.send("Ups, a santa se le cayo un regalo", file=utils.discord.File(fp="resource/img/Gift-Box.jpg"), view=view)
+        
+        await view.wait()
+        if view.received_gift == False:
+            await message.delete()
     
     
 def setup(bot):
