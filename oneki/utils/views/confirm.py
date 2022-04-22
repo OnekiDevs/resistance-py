aabe2a283533831c03c15bf57d9e8166946e837c
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-04-21 21:20:37 -0500

    fixed error in confirm view

--- oneki/utils/views/confirm.py
+++ oneki/utils/views/confirm.py
@@ -10,14 +10,14 @@ class View(discord.ui.View):
     # stop the View from listening to more input.
     # We also send the user an ephemeral message that we're confirming their choice.
     @discord.ui.button(label='Confirm', style=discord.ButtonStyle.red)
-    async def confirm(self, button: discord.ui.Button, interaction: discord.Interaction):
+    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.send_message('Confirming', ephemeral=True)
         self.value = True
         self.stop()
 
     # This one is similar to the confirmation button except sets the inner value to `False`
     @discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
-    async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
+    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.send_message('Cancelling', ephemeral=True)
         self.value = False
         self.stop()
