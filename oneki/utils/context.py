e478bba696835bde230e5014849999e026e221df
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-06-14 00:38:29 -0500

    changes in the context

--- oneki/utils/context.py
+++ oneki/utils/context.py
@@ -1,12 +1,11 @@
 import aiohttp
 import discord
 import datetime
-from typing import Optional, Union
 from discord.ext import commands
 from utils.db import AsyncClient
+    
 
-
-class BaseContext:
+class Context(commands.Context):
     @property
     def db(self) -> AsyncClient:
         return self.bot.db
@@ -33,26 +32,5 @@ class BaseContext:
 
         print(f"log: \n{message}\ncommand: {self.command}\ntimestamp: {timestamp}")
         await self.debug_channel.send(f"log: \n`{message}`\ncommand: `{self.command}`\ntimestamp: `{timestamp}`")
-    
-
-class Context(commands.Context, BaseContext):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
         
-
-class Ctx(BaseContext):
-    def __init__(self, author, channel, command, bot) -> None:
-        self.author: Union[discord.User, discord.Member] = author
-        self.guild: Optional[discord.Guild] = channel.guild
-        self.channel: discord.abc.MessageableChannel = channel
-        self.command = command
-        self.bot = bot
-        
-    @classmethod
-    def from_context(cls, ctx: Context):
-        return cls(ctx.author, ctx.channel, ctx.command, ctx.bot)
-        
-    @classmethod
-    def from_interaction(cls, interaction: discord.Interaction):
-        return cls(interaction.user, interaction.channel, interaction.command, interaction.client)
     
