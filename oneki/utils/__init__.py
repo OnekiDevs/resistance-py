d109b4c628366b7dee6b69d0d03cf6dded5bb222
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-06-15 22:27:43 -0500

    slight changes in utils

--- oneki/utils/__init__.py
+++ oneki/utils/__init__.py
@@ -1,16 +1,15 @@
-# Librerias importantes
 import discord
 from discord import app_commands
 from discord.ext import commands
 
-# Etc
 import datetime
 import asyncio 
 import random
 import re
 
+from .cog import Cog
 
-# Funciones utiles
+
 utcnow = datetime.datetime.utcnow
 is_empty = lambda data_structure: False if data_structure else True
 
@@ -24,11 +23,4 @@ async def send_file_and_get_url(bot, file: discord.File):
     channel = await bot.fetch_channel(885674115946643456)
     message = await channel.send(file=file) 
     return message.attachments[0]
-
-async def delete_collection(collection_ref):
-    async for doc_ref in collection_ref.list_documents():
-        async for subcollection_ref in doc_ref.collections():
-            async for doc in subcollection_ref.list_documents():
-                await doc.delete()
-
-        await doc_ref.delete()
+    
