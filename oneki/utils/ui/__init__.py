876be6d93279d4128d90cb81abe7b454a4902177
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-06-16 14:23:04 -0500

    import other classes from discord.ui

--- oneki/utils/ui/__init__.py
+++ oneki/utils/ui/__init__.py
@@ -1,2 +1,4 @@
+import discord
+from discord.ui import Button, Select, Modal, TextInput
 from .view import View, ExitableView, CancellableView
 from .decorators import button, select, change_color_when_used, disable_when_pressed
