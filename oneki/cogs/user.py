3a6f0950587bc2955ed8d1e8015717ee240825d9
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-06-15 22:28:55 -0500

    updated user cog

--- oneki/cogs/user.py
+++ oneki/cogs/user.py
@@ -12,9 +12,9 @@ def avatar_embed(ctx, member, translation):
     avatar = member.guild_avatar.url if member.guild_avatar is not None else member.avatar.url
     
     embed = utils.discord.Embed(colour=member.color, timestamp=utils.utcnow())
-    embed.set_author(name=translation["embed"]["author"].format(member), url=avatar)
+    embed.set_author(name=translation.embed.author.format(member), url=avatar)
     embed.set_image(url=avatar)
-    embed.set_footer(text=translation["embed"]["footer"].format(ctx.author.name), icon_url=ctx.author.avatar.url)
+    embed.set_footer(text=translation.embed.footer.format(ctx.author.name), icon_url=ctx.author.avatar.url)
     
     return embed
 
@@ -23,7 +23,7 @@ def info_embed(ctx, member, translation):
     roles = "".join([role.mention for role in member.roles])
 
     embed = utils.discord.Embed(
-        title=translation["embed"]["title"],
+        title=translation.embed.title,
         description=roles,
         color=member.color,
         timestamp=utils.utcnow(), 
@@ -33,15 +33,15 @@ def info_embed(ctx, member, translation):
     
     if member.activity is not None: 
         activity = member.activity if isinstance(member.activity, utils.discord.CustomActivity) else member.activity.name
-        embed.add_field(name=translation["embed"]["fields"][0], value=f"```{activity}```", inline=False)
+        embed.add_field(name=translation.embed.fields[0], value=f"```{activity}```", inline=False)
     
-    embed.add_field(name=translation["embed"]["fields"][1], value=f"```{member.created_at}```")
-    embed.add_field(name=translation["embed"]["fields"][2], value=f"```{member.joined_at}```")
-    embed.add_field(name=translation["embed"]["fields"][3], value=f"```{member.color}```")
-    embed.add_field(name=translation["embed"]["fields"][4], value=f"```{member.id}```")
-    embed.add_field(name=translation["embed"]["fields"][5], value=f"```{member.raw_status}```")
+    embed.add_field(name=translation.embed.fields[1], value=f"```{member.created_at}```")
+    embed.add_field(name=translation.embed.fields[2], value=f"```{member.joined_at}```")
+    embed.add_field(name=translation.embed.fields[3], value=f"```{member.color}```")
+    embed.add_field(name=translation.embed.fields[4], value=f"```{member.id}```")
+    embed.add_field(name=translation.embed.fields[5], value=f"```{member.raw_status}```")
     
-    embed.set_footer(text=translation["embed"]["footer"].format(ctx.author.name), icon_url=ctx.author.avatar.url)
+    embed.set_footer(text=translation.embed.footer.format(ctx.author.name), icon_url=ctx.author.avatar.url)
     
     return embed
 
@@ -70,9 +70,9 @@ class Profile(ui.ExitableView):
             banner = member.banner.url
         
         embed = utils.discord.Embed(colour=member.color, timestamp=utils.utcnow())
-        embed.set_author(name=self.translations["embed"]["author"].format(member))
+        embed.set_author(name=self.translations.embed.author.format(member))
         embed.set_image(url=banner)
-        embed.set_footer(text=self.translations["embed"]["footer"].format(self.ctx.author.name), icon_url=self.ctx.author.avatar.url)
+        embed.set_footer(text=self.translations.embed.footer.format(self.ctx.author.name), icon_url=self.ctx.author.avatar.url)
         
         return embed
     
@@ -88,9 +88,9 @@ class Profile(ui.ExitableView):
         banner = self.member.banner.url if self.member.banner is not None else self.DEFAULT_BANNER
         
         embed = utils.discord.Embed(colour=self.member.color, timestamp=utils.utcnow())
-        embed.set_author(name=translation["embed"]["author"].format(self.member), url=banner)
+        embed.set_author(name=translation.embed.author.format(self.member), url=banner)
         embed.set_image(url=banner)
-        embed.set_footer(text=translation["embed"]["footer"].format(self.ctx.author.name), icon_url=self.ctx.author.avatar.url)
+        embed.set_footer(text=translation.embed.footer.format(self.ctx.author.name), icon_url=self.ctx.author.avatar.url)
 
         await interaction.response.edit_message(embed=embed, view=self)
         
@@ -101,9 +101,9 @@ class Profile(ui.ExitableView):
         await interaction.response.edit_message(embed=embed, view=self)
         
         
-class User(utils.commands.Cog):
+class User(utils.Cog):
     def __init__(self, bot):
-        self.bot = bot
+        super().__init__(bot)
         self.afks = {}
         
     async def cog_load(self):
@@ -170,25 +170,25 @@ class User(utils.commands.Cog):
                 await member.edit(nick=member.display_name.replace("[AFK] ", ""))
             except: pass
             
-            embed = utils.discord.Embed(title=translation["no_longer_afk"].format(member.display_name), color=0xFCE64C)
+            embed = utils.discord.Embed(title=translation.no_longer_afk.format(member.display_name), color=0xFCE64C)
             return await ctx.send(embed=embed)
             
-        reason = reason or ctx.translation["no_reason"]
+        reason = reason or ctx.translation.no_reason
         if len(reason) > 50: 
-            return await ctx.send(ctx.translation["too_long"])
+            return await ctx.send(ctx.translation.too_long)
         
         if utils.check_links(reason): 
-            return await ctx.send(ctx.translation["no_links"])
+            return await ctx.send(ctx.translation.no_links)
 
         await self.add_to_afk(member.id, reason=reason)
-        embed = utils.discord.Embed(title=ctx.translation["embed"]["title"].format(member.display_name), color=0x383FFF)
+        embed = utils.discord.Embed(title=ctx.translation.embed.title.format(member.display_name), color=0x383FFF)
         if len(member.display_name) >= 27: 
-            ctx.send(ctx.translation["max_name_length"].format(member.mention))
+            ctx.send(ctx.translation.max_name_length.format(member.mention))
         else: 
             try:
                 await member.edit(nick=f"[AFK] {member.display_name}")
             except utils.discord.errors.Forbidden:
-                await ctx.send(ctx.translation["no_permissions"])
+                await ctx.send(ctx.translation.no_permissions)
                 
         await ctx.send(embed=embed)
     
@@ -208,7 +208,7 @@ class User(utils.commands.Cog):
                 await member.edit(nick=member.display_name.replace("[AFK] ", ""))
             except: pass
             
-            embed = utils.discord.Embed(title=translation["no_longer_afk"].format(member.display_name), color=0xFCE64C)
+            embed = utils.discord.Embed(title=translation.no_longer_afk.format(member.display_name), color=0xFCE64C)
             await message.channel.send(embed=embed, delete_after=10.0)
 
         # is there a mention of an afk user?
@@ -218,8 +218,8 @@ class User(utils.commands.Cog):
                 if str(user.id) in self.afks:
                     data = self.afks[str(user.id)]
                     embed = utils.discord.Embed(
-                        title=translation["embed"]["title"].format(user.display_name),
-                        description=translation["embed"]["reason"].format(data["reason"]),
+                        title=translation.embed.title.format(user.display_name),
+                        description=translation.embed.reason.format(data["reason"]),
                         timestamp=data["time"],
                         color=0xFCE64C
                     )
