dac2acb39f3c1c6ad9996851adf60e57a3225ab4
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-06-16 14:21:50 -0500

    changes in confirm view

--- oneki/utils/ui/confirm.py
+++ oneki/utils/ui/confirm.py
@@ -1,7 +1,7 @@
 import discord
 from view import View
 import decorators 
-from typing import Coroutine, Any
+from typing import Coroutine, Any, Optional
 
 
 class View(View):
@@ -17,7 +17,7 @@ class View(View):
         view.NAME = ""
         view.start()
     """
-    def __init__(self, context = None, *, confirmed: Coroutine[Any, Any, dict], cancelled: Coroutine[Any, Any, dict]):
+    def __init__(self, context = None, *, confirmed: Optional[Coroutine[Any, Any, dict]] = None, cancelled: Optional[Coroutine[Any, Any, dict]] = None):
         super().__init__(context)
         self.value = None
         
@@ -27,11 +27,21 @@ class View(View):
     @decorators.button(label="Confirm", style=discord.ButtonStyle.red)
     @decorators.disable_when_pressed
     async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button, translation):
+        await interaction.response.send_message("Confirming", ephemeral=True)
         self.value = True
-        return await self.confirmed(translation)
+        
+        if self.confirmed is not None:
+            return await self.confirmed(translation)
+        
+        return {}
 
     @decorators.button(label="Cancel", style=discord.ButtonStyle.grey)
     @decorators.disable_when_pressed
     async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button, translation):
+        await interaction.response.send_message("Cancelling", ephemeral=True)
         self.value = False
-        return await self.cancelled(translation)
+        
+        if self.cancelled is not None:
+            return await self.cancelled(translation)
+        
+        return {}
