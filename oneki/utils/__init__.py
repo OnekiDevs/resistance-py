4ff7e7b2f4ccf3d8dff5e0fc5a311cc86adfbdec
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-06-19 02:18:11 -0500

    function filled_bar added, return a bar of text

--- oneki/utils/__init__.py
+++ oneki/utils/__init__.py
@@ -3,6 +3,7 @@ from discord import app_commands
 from discord.ext import commands
 
 import datetime
+import math
 import asyncio 
 import random
 import re
@@ -18,6 +19,10 @@ def check_links(string):
     return re.findall(regex, string)
 
 color_hex = lambda _hex: int(hex(int(_hex.replace("#", ""), 16)), 0)
+
+def filled_bar(current: int, *, len = 25) -> str:
+    progress = math.floor((current / 100) * len)
+    return "".join(["█" for _ in range(0, progress)]) + "".join([" " for _ in range(0, len - progress)])
 
 async def send_file_and_get_url(bot, file: discord.File):
     channel = await bot.fetch_channel(885674115946643456)
