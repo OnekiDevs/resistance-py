b681f9d21d8a405aed385741c215d3cc757aabf2
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-06-27 01:35:13 -0500

    the types of self.bot and self.translations are now specified

--- oneki/utils/cog.py
+++ oneki/utils/cog.py
@@ -1,8 +1,13 @@
 from discord.ext import commands
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from bot import OnekiBot
+    from .translations import Translations
 
 
 class Cog(commands.Cog):
     def __init__(self, bot) -> None:
-        self.bot = bot
-        self.translations = bot.translations
-    
+        self.bot: OnekiBot = bot
+        self.translations: Translations = bot.translations
+        
