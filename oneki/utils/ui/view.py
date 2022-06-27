d76062ae9fd386c895f514327617e350d7c273d9
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-06-27 01:42:06 -0500

    use of ReportBug apart from slight changes

--- oneki/utils/ui/view.py
+++ oneki/utils/ui/view.py
@@ -1,8 +1,10 @@
 import discord
 from discord import ui 
 from ..context import Context
-from ..translations import Translations, Translation
-from typing import Optional, Union
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ..translations import Translation
 
 
 def _can_be_disabled(item):
@@ -20,18 +22,17 @@ class View(ui.View):
         self.msg: Optional[discord.Message] = None
         self.embed: Optional[discord.Embed] = None
         
-        self.translations: Optional[Translations] = None
+        self.translations: Optional[Translation] = None
         
         self.kwargs = kwargs
-        self._disabled = False
         
     async def get_data(self, **kwargs): 
         return (None,)
         
-    async def get_content(self, *args) -> str:
+    async def get_content(self, *args) -> Optional[str]:
         return None
         
-    async def get_embed(self, *args) -> discord.Embed:
+    async def get_embed(self, *args) -> Optional[discord.Embed]:
         return None # this is optional to implement
         
     async def update_components(self, *args):
@@ -39,18 +40,18 @@ class View(ui.View):
         
     async def process_data(self):
         data = await self.get_data(**self.kwargs)
-    
+        data = data or (None,)
+        
         content = await self.get_content(*data)
         self.embed = await self.get_embed(*data)
         await self.update_components(*data)
         
         kwargs = {"content": content, "embed": self.embed, "view": self}
-        
         return kwargs
         
     async def start(self, interaction: Optional[discord.Interaction] = None, *, ephemeral = False):
         if self.name is not None:
-            self.translations: Translation = interaction.client.translations.view(interaction.locale.value.split("-")[0], self.name) if interaction is not None else self.ctx.cog.translations.view(self.ctx.lang, self.name)
+            self.translations: Translation = interaction.client.translations.view(interaction.locale.value, self.name) if interaction is not None else self.ctx.cog.translations.view(self.ctx.lang, self.name)
         
         kwargs = await self.process_data()
         kwargs["ephemeral"] = ephemeral
@@ -75,15 +76,23 @@ class View(ui.View):
                 item.disabled = True
         
     async def disable(self, **kwargs):
-        if self._disabled:
+        if self.is_finished():
             return
         
+        self.stop()
         self._disable_children()
         
         if self.msg is None:
             raise RuntimeError("can't disable view without start")
 
         await self.msg.edit(view=self, **kwargs)
+        
+    async def on_error(self, interaction: discord.Interaction, error: Exception, item) -> None:
+        await super().on_error(interaction, error, item)
+        
+        from .report_bug import ReportBug
+        view = ReportBug(error=error)
+        await view.start(interaction)
         
     async def on_timeout(self) -> None:
         await self.disable()
