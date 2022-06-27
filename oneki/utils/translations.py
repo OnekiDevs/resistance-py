dc92b5e3d589df75c9f6c7783910a34b8a4ddeca
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-06-27 01:32:44 -0500

    return types are now specified

--- oneki/utils/translations.py
+++ oneki/utils/translations.py
@@ -42,7 +42,7 @@ class Translations:
         
         return cls(translations)
             
-    def _get_translations(self, lang, *, type, name):
+    def _get_translations(self, lang, *, type, name) -> Translation:
         """
         Command = TypeTranslation.command;
         View = TypeTranslation.view
@@ -50,23 +50,24 @@ class Translations:
         Function = TypeTranslation.func
         """ 
         _name = type.value + "_" + name
+        lang = lang.split("-")[0]
         
         default_translation = self._translations[DEFAULT_LANGUAGE][_name]
         translation = self._translations[lang].get(_name, default_translation)
         return translation
     
-    def command(self, lang, command_name):
+    def command(self, lang, command_name) -> Translation:
         command_translations = self._get_translations(lang, type=TypeTranslation.command, name=command_name)
         return command_translations
         
-    def view(self, lang, interaction_name):
+    def view(self, lang, interaction_name) -> Translation:
         command_translations = self._get_translations(lang, type=TypeTranslation.view, name=interaction_name)
         return command_translations
     
-    def event(self, lang, event_name):
+    def event(self, lang, event_name) -> Translation:
         event_translations = self._get_translations(lang, type=TypeTranslation.event, name=event_name)
         return event_translations
     
-    def function(self, lang, function_name):
+    def function(self, lang, function_name) -> Translation:
         function_translations = self._get_translations(lang, type=TypeTranslation.func, name=function_name)
         return function_translations
