a86aaa225af977a585c6a0a1b396ce6a521eaef6
Gael <69738055+Gael-devv@users.noreply.github.com>
2022-06-27 01:42:49 -0500

    import new model class and ReportBug view

--- oneki/utils/ui/__init__.py
+++ oneki/utils/ui/__init__.py
@@ -1,3 +1,5 @@
-from discord.ui import Button, Select, Modal, TextInput
+from discord.ui import Button, Select, TextInput
+from .modal import Modal
 from .view import View, ExitableView, CancellableView
+from .report_bug import ReportBug
 from .decorators import button, select, change_color_when_used, disable_when_pressed
